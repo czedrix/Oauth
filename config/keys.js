77f3d82f4657a13d93c05647590d351efa4ea43b
czedrix <barcena.it111@mgail.com>
2020-04-09 15:19:29 +0800

    part1 commit

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    google:{
+        clientID: '579825616749-mntvfqigqd8htjdb882gl35vvac92vqk.apps.googleusercontent.com',
+        clientSecret: 'ciXa5LR3R0LPvEBWtYPwH-zW'
+    }
+}
